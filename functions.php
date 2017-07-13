9f73d3b92346a9ac5d40f5141cbddb897eeda577
Nate Finch <finchproservices@gmail.com>
2017-07-13 15:49:25 -0500

    commit to open new branch

--- functions.php
+++ functions.php
@@ -108,7 +108,7 @@ add_theme_support( 'custom-background' );
 add_theme_support( 'genesis-after-entry-widget-area' );
 
 // Add support for 3-column footer widgets.
-add_theme_support( 'genesis-footer-widgets', 3 );
+add_theme_support( 'genesis-footer-widgets', 4 );
 
 // Add Image Sizes.
 add_image_size( 'featured-image', 720, 400, TRUE );
