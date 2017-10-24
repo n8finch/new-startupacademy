c7596f275bd7f575eae9cc3c0d333597041e21c1
Nate Finch <finchproservices@gmail.com>
2017-10-24 18:35:25 -0500

    move coaches masonry part to template parts

--- front-page.php
+++ front-page.php
@@ -217,41 +217,6 @@ function one_pager_homepage_content() {
 			<a class="button button-orange" href="<?php echo $acf_fields['homepage_learn_button_link']; ?>">
 				<?php echo $acf_fields['homepage_learn_button_text']; ?>
 			</a>
-			<div class="homepage-coaches-container">
-				<?php
-				$args = array(
-					'orderby'          => 'rand',
-					'order'            => 'DESC',
-					'post_type'        => 'coach',
-					'post_status'      => 'publish',
-					'posts_per_page'   => 12,
-					'suppress_filters' => true
-				);
-				$coaches = get_posts( $args );
-				if( $coaches ) {
-					$counter = 1;
-					foreach( $coaches as $coach ) {
-						$coach_name = $coach->post_title;
-						$coach_excerpt = $coach->post_excerpt;
-						$coach_pic = get_the_post_thumbnail_url($coach->ID);
-						$coach_url = get_the_permalink($coach->ID);
-						$counter > 6 ? $offset_class = 'offset-class' : '';
-						?>
-						<a href="<?php echo $coach_url; ?>">
-							<div class="coach-card <?php echo $offset_class; ?>">
-								<img class="coach-image" src="<?php echo $coach_pic?>" />
-								<div>
-									<div class="coach-name"><?php echo $coach_name; ?></div>
-									<div class="coach_excerpt"><?php echo $coach_excerpt; ?></div>
-								</div>
-							</div>
-						</a>
-						<?php
-						$counter++;
-					}
-				}
-				?>
-			</div>
 		</div>
 	</section>
 
